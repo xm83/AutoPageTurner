3101592abc5f46351c9d2379b95ac12fedadf4fd
Yan <hy296@DHLABP10MAC.central.yale.internal>
2019-12-01 14:18:13 -0500

    Improve last row segmentation with computer vision cropping out white space

--- app/last_row.py
+++ app/last_row.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def lastRow(gray_im):
+    inverted = 255*(gray_im < 128).astype(np.uint8) # To invert the text to white
+    coords = cv2.findNonZero(inverted) # Find all non-zero points (text)
+    x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
+    rect = gray_im[y:y+h, x:x+w]
+    cv2.imwrite("rect.png", rect)
+
     rowStepSize = 40
     bandHeight = 200
     lasti = 0
@@ -10,6 +16,7 @@ def lastRow(gray_im):
     subsums = []
 
     # shape of image is height by width
+    gray_im = rect
     height = gray_im.shape[0]
     
     for i in range(height, int(height/2), -rowStepSize): #chang middle term back to 0
@@ -25,10 +32,10 @@ def lastRow(gray_im):
             break
     
     if lasti > 0:
-    	wantedFirstRow = height - rowStepSize * lasti - bandHeight
-    	if wantedFirstRow >= 0:
+        wantedFirstRow = height - rowStepSize * lasti - bandHeight
+        if wantedFirstRow >= 0:
             wantedImg = gray_im[height - rowStepSize * lasti - bandHeight: height - rowStepSize * (lasti-1), :]
+            cv2.imwrite("lastRow.png", wantedImg)
             return wantedImg
-   
 
         
