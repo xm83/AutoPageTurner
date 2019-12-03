8943522571b564fde11cb36bd546821a68c4bfe3
Sally Ma <sally.ma@yale.edu>
2019-12-03 09:30:18 -0500

    better audio

--- app/audio_sheet_comparison.py
+++ app/audio_sheet_comparison.py
@@ -52,8 +52,8 @@ def file_compare(file, sheet):
 	log_cqt = librosa.amplitude_to_db(np.abs(cqt))
 	a = []
 	for i in range(len(log_cqt.argmax(0))):
-	    k = log_cqt.argmax(0)[i]
-	    a.append(k+24)
+		k = log_cqt.argmax(0)[i]
+		a.append(k+24)
 	audio = audio_pro.smooth_audio(a)
 
 	#sheet process
@@ -63,17 +63,14 @@ def file_compare(file, sheet):
 	return audio_sheet_compare(audio, sheet)
 
 def realtime_pitch(stream, buffer_size):
-    audiobuffer = stream.read(buffer_size, exception_on_overflow = False)
-    signal = np.fromstring(audiobuffer, dtype=np.float32)
-    bins_per_octave = 12
-    cqt = librosa.cqt(signal, sr=44100, n_bins=60, bins_per_octave=bins_per_octave)
-    log_cqt = librosa.amplitude_to_db(np.abs(cqt))
-    return log_cqt.argmax(0)[1]
+	audiobuffer = stream.read(buffer_size, exception_on_overflow = False)
+	signal = np.fromstring(audiobuffer, dtype=np.float32)
+	bins_per_octave = 12
+	cqt = librosa.cqt(signal, sr=44100, n_bins=60, bins_per_octave=bins_per_octave)
+	log_cqt = librosa.amplitude_to_db(np.abs(cqt))
+	return log_cqt.argmax(0)[1]
 
 def stream_compare(sheet):
-	# time.sleep(4)
-	# return True
-
 	# initialise pyaudio
 	# sheet 
 	midi = note_to_midi(sheet)
@@ -87,11 +84,7 @@ def stream_compare(sheet):
 	n_channels = 1
 	sr = 44100
 	record_duration = 5 # exit
-	stream = p.open(format=pyaudio_format,
-	                channels=n_channels,
-	                rate=sr,
-	                input=True,
-	                frames_per_buffer=buffer_size)
+	stream = p.open(format=pyaudio_format, channels=n_channels, rate=sr, input=True, frames_per_buffer=buffer_size)
 
 	# print(sheet)
 
@@ -101,44 +94,45 @@ def stream_compare(sheet):
 	ratio = None
 
 	while True:
-	    try:
-	        s = realtime_pitch(stream, buffer_size)
-	        while s!= sheet[0, 0]: s = realtime_pitch(stream, buffer_size)
-	        
-	        while index < sheet.shape[0]:
-	            l = 0
-	            while s == sheet[index, 0]: 
-	                l+=1
-	                s = realtime_pitch(stream, buffer_size)
-	            
-	            t = math.ceil(l/3)
-	            c = 0
-	            while c < t:
-	                s = realtime_pitch(stream, buffer_size)
-	                c += 1
-	                if index + 1 < sheet.shape[0] and s == sheet[index+1, 0]: 
-	                    break
-	                if s == sheet[index, 0]:
-	                    while s == sheet[index, 0]: 
-	                        l+=1
-	                        s = realtime_pitch(stream, buffer_size)
-	                    c = 0
-	            
-	            if ratio == None: 
-	                ratio = l/sheet[index, 1]
-	                index += 1
-	            elif np.abs((ratio-l/sheet[index, 1])/ratio) < 0.5:
-	                if index == sheet.shape[0] -1: 
-	                    return True
-	                else: 
-	                    index += 1
-	            else: 
-	            	if index == sheet.shape[0] -1 and l > 15: return True
-	            	index = 0
-	            	break
-	    except KeyboardInterrupt:
-	        print("*** Ctrl+C pressed, exiting")
-	        break
+		try:
+			ratio = None
+			s = realtime_pitch(stream, buffer_size)
+			while s!= sheet[0, 0]: s = realtime_pitch(stream, buffer_size)
+
+			while index < sheet.shape[0]:
+				l = 0
+				while s == sheet[index, 0]:
+					l+=1
+					s = realtime_pitch(stream, buffer_size)
+				if ratio != None: t = math.ceil(sheet[index, 1]*ratio/5)
+				else: t = math.ceil(l/5)
+
+				c = 0
+				while c < t:
+					s = realtime_pitch(stream, buffer_size)
+					c += 1
+					if index + 1 < sheet.shape[0] and s == sheet[index+1, 0]: break
+					if s == sheet[index, 0]:
+						while s == sheet[index, 0]: 
+							l+=1
+							s = realtime_pitch(stream, buffer_size)
+						c = 0
+
+				if ratio == None: 
+					ratio = l/sheet[index, 1]
+					index += 1
+				elif np.abs((ratio-l/sheet[index, 1])/ratio) < 0.5:
+					if index == sheet.shape[0] -1: 
+						return True
+					else: 
+						index += 1
+				else: 
+					if index == sheet.shape[0] -1 and l > 15: return True
+					index = 0
+					break
+		except KeyboardInterrupt:
+			print("*** Ctrl+C pressed, exiting")
+			break
 
 	print("*** done recording")
 	stream.stop_stream()
