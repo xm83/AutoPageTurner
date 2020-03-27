c86ec491f2987c189a82fc34c0f39f1e0c226b73
Annie Gao <anniegao2007@gmail.com>
2020-03-27 00:12:11 -0400

    cut network_sheet_spec and added recurrent layer

--- version_2/experiments/rl_score_following_game/score_following_game/recurrent_approach/networks_sheet_spec.py
+++ version_2/experiments/rl_score_following_game/score_following_game/recurrent_approach/networks_sheet_spec.py
@@ -8,7 +8,7 @@ from score_following_game.agents.networks_utils import weights_init, num_flat_fe
 
 class ScoreFollowingNetMSMDLCHSDeepDoLight(nn.Module):
 
-    def __init__(self,  n_actions, perf_shape, score_shape):
+    def __init__(self,  n_actions, perf_shape, score_shape, rnn_hidden_dim=12, num_recurrent_layers=1):
         super(ScoreFollowingNetMSMDLCHSDeepDoLight, self).__init__()
 
         # spec part
@@ -49,13 +49,10 @@ class ScoreFollowingNetMSMDLCHSDeepDoLight(nn.Module):
         # multi-modal part
         self.concat_fc = nn.Linear(512 + 512, 512)
 
-        self.policy_fc = nn.Linear(self.concat_fc.out_features, 256)
-        self.policy_fc_do = nn.Dropout(p=0.2)
-        self.policy_out = nn.Linear(self.policy_fc.out_features, n_actions)
-
-        self.critic_fc = nn.Linear(self.concat_fc.out_features, 256)
-        self.critic_fc_do = nn.Dropout(p=0.2)
-        self.critic_out = nn.Linear(self.critic_fc.out_features, 1)
+        # recurrent layer
+        self.rnn = nn.RNN(512, rnn_hidden_dim, num_recurrent_layers, batch_first=True)   
+        # fully connected layer
+        self.final_fc = nn.Linear(hidden_dim, 2)
 
         self.apply(weights_init)
 
@@ -93,70 +90,11 @@ class ScoreFollowingNetMSMDLCHSDeepDoLight(nn.Module):
 
         cat_x = F.elu(self.concat_fc(cat_x))
 
-        policy = F.elu(self.policy_fc(cat_x))
-        policy = self.policy_fc_do(policy)
-        policy = self.policy_out(policy)
-
-        critic = F.elu(self.critic_fc(cat_x))
-        critic = self.critic_fc_do(critic)
-        critic = self.critic_out(critic)
-
-        # if we use softmax and the logarithmic softmax PyTorch recommends to use the log_softmax function
-        # therefore we return the linear output of the policy and apply the softmax/log softmax where it is necessary
-        return {'policy': {'logits': policy}, 'value': critic}
-
-
-class ScoreFollowingNetNottinghamLS(nn.Module):
-
-    def __init__(self,  n_actions, perf_shape, score_shape):
-        super(ScoreFollowingNetNottinghamLS, self).__init__()
-
-        self.spec_conv1 = nn.Conv2d(perf_shape[0], out_channels=16, kernel_size=3, stride=2)
-        self.spec_conv2 = nn.Conv2d(self.spec_conv1.out_channels, out_channels=32, kernel_size=3, stride=2)
-        self.spec_conv3 = nn.Conv2d(self.spec_conv2.out_channels, out_channels=32, kernel_size=3, stride=2)
-        self.spec_conv4 = nn.Conv2d(self.spec_conv3.out_channels, out_channels=64, kernel_size=3, stride=1)
-
-        self.sheet_conv1 = nn.Conv2d(score_shape[0], out_channels=16, kernel_size=5, stride=(1, 2))
-        self.sheet_conv2 = nn.Conv2d(self.sheet_conv1.out_channels, out_channels=32, kernel_size=3, stride=2)
-        self.sheet_conv3 = nn.Conv2d(self.sheet_conv2.out_channels, out_channels=32, kernel_size=3, stride=2)
-        self.sheet_conv4 = nn.Conv2d(self.sheet_conv3.out_channels, out_channels=64, kernel_size=3, stride=(1, 2))
-
-        self.concat_fc = nn.Linear(self.spec_conv4.out_channels * 6 * 2 + self.sheet_conv4.out_channels * 6 * 8, 256)
-
-        self.policy_fc = nn.Linear(self.concat_fc.out_features, 256)
-        self.policy_out = nn.Linear(self.policy_fc.out_features, n_actions)
-
-        self.critic_fc = nn.Linear(self.concat_fc.out_features, 256)
-        self.critic_out = nn.Linear(self.critic_fc.out_features, 1)
-
-        self.apply(weights_init)
-
-    def forward(self, perf, score):
-
-        spec_x = F.elu(self.spec_conv1(perf))
-        spec_x = F.elu(self.spec_conv2(spec_x))
-        spec_x = F.elu(self.spec_conv3(spec_x))
-        spec_x = F.elu(self.spec_conv4(spec_x))
-
-        spec_x = spec_x.view(-1, num_flat_features(spec_x)) # flatten
-
-        sheet_x = F.elu(self.sheet_conv1(score))
-        sheet_x = F.elu(self.sheet_conv2(sheet_x))
-        sheet_x = F.elu(self.sheet_conv3(sheet_x))
-        sheet_x = F.elu(self.sheet_conv4(sheet_x))
-
-        sheet_x = sheet_x.view(-1, num_flat_features(sheet_x)) # flatten
-
-        cat_x = torch.cat((spec_x, sheet_x), dim=1)
-
-        cat_x = F.elu(self.concat_fc(cat_x))
-
-        policy = F.elu(self.policy_fc(cat_x))
-        policy = self.policy_out(policy)
-
-        critic = F.elu(self.critic_fc(cat_x))
-        critic = self.critic_out(critic)
-
-        # if we use softmax and the logarithmic softmax PyTorch recommends to use the log_softmax function
-        # therefore we return the linear output of the policy and apply the softmax/log softmax where it is necessary
-        return {'policy': {'logits': policy}, 'value': critic}
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, _ = self.rnn(x, cat_x)
+        
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = out.contiguous().view(-1, self.rnn_hidden_dim)
+        out = self.final_fc(out)
+        
+        return out
