bfb5b4e69eeb85192f09a26cd9df4af19de60b95
Annie Gao of group cpsc424 <cpsc424_alg76@c04n06.grace.hpc.yale.internal>
2020-04-12 18:53:45 -0400

    print statement

--- version_2/experiments/rl_score_following_game/grace_init.sh
+++ version_2/experiments/rl_score_following_game/grace_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 srun --pty --cpus-per-task=1 --mem-per-cpu=6100mb --partition=cpsc424 -t 6:00:00 bash
+echo "started an interactive session"
 module load miniconda
 module load FFmpeg/3.4.2-foss-2018a
 source activate score_following
